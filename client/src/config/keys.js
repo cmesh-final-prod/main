36e514d16772adae3d92c00c52334dc5049584f5
Chaitanya <circlemesh@gmail.com>
2018-07-19 11:24:34 -0700

    fixed the .config/prod typo

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('.config/prod.js');
+  module.exports = require('config/prod.js');
 } else if (process.env.NODE_ENV === 'test') {
   module.exports = require('config/test');
 } else {
