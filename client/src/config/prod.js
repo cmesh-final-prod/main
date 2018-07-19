146c3471fc39d46fcc42cec9366ad6927a8c0524
Chaitanya <circlemesh@gmail.com>
2018-07-19 11:29:35 -0700

    added pubnub subscribe key as a string in config/prod

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-  pubnubPublishKey: process.env.PUBNUB_PUBLISH_KEY,
-  pubnubSubscribeKey: process.env.PUBNUB_SUBSCRIBE_KEY
+  pubnubSubscribeKey: 'sub-c-c280e022-8b3f-11e8-bb1a-4ed21a5fe7f3'
 };
