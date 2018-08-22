6d747606ac23bbad8d16f8de663e68af70cd1b5c
Chaitanya <circlemesh@gmail.com>
2018-08-22 16:05:15 -0700

    bg img

--- client/src/components/web/_misc/Terms.js
+++ client/src/components/web/_misc/Terms.js
@@ -7,6 +7,7 @@ const Terms = () => {
         <iframe
           src="https://app.termly.io/document/privacy-policy/e35ab499-6fed-464d-8f46-2434c4a2f7db"
           frameborder="0"
+          title="privacy policy"
           allowfullscreen
         >
           <p>Your browser does not support iframes.</p>
@@ -17,6 +18,7 @@ const Terms = () => {
         <iframe
           src="https://app.termly.io/document/terms-of-use-for-website/c46cf1de-89f4-45f0-b40f-70713a9b46e2"
           frameborder="0"
+          title="terms of use"
           allowfullscreen
         >
           <p>Your browser does not support iframes.</p>
@@ -27,6 +29,7 @@ const Terms = () => {
         <iframe
           src="https://app.termly.io/document/cookie-policy/c9af3771-5924-4208-9730-5561a9f6c331"
           frameborder="0"
+          title="cookie policy"
           allowfullscreen
         >
           <p>Your browser does not support iframes.</p>
@@ -37,6 +40,7 @@ const Terms = () => {
         <iframe
           src="https://app.termly.io/document/disclaimer/09c23119-17ee-408c-b199-8889ca9711b2"
           frameborder="0"
+          title="disclaimer"
           allowfullscreen
         >
           <p>Your browser does not support iframes.</p>
