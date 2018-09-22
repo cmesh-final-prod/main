3e15eb123ab75be9cc88380ee5334627d5379d12
Chaitanya <circlemesh@gmail.com>
2018-09-22 13:40:22 -0700

    circlemesh 2.0 first complete commit

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,3 +1,6 @@
 module.exports = {
-  pubnubSubscribeKey: 'sub-c-c280e022-8b3f-11e8-bb1a-4ed21a5fe7f3'
+  // test-prod
+  // pubnubSubscribeKey: 'sub-c-c280e022-8b3f-11e8-bb1a-4ed21a5fe7f3'
+  // final-prod
+  pubnubSubscribeKey: "sub-c-6e4a7f0e-be9d-11e8-a0ae-c2e67adadb66"
 };
