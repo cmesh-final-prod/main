db2c29dcc53430b211bec9198e5c98c84b1f7581
Chaitanya <circlemesh@gmail.com>
2018-09-23 14:26:38 -0700

    added android location services

--- client/src/components/locationError/Instructions.js
+++ client/src/components/locationError/Instructions.js
@@ -1,44 +1,54 @@
-import React, { Component } from 'react';
-import * as L from 'components/_misc/LOG-TYPES';
+import React, { Component } from "react";
+import * as L from "components/_misc/LOG-TYPES";
 import {
   isSafari,
   isAndroid,
   isChrome,
   isFirefox,
   isOpera
-} from 'react-device-detect';
+} from "react-device-detect";
 
 // importing components
-import Oops from 'components/locationError/Oops';
+import Oops from "components/locationError/Oops";
 
 // importing _hoc
-import withLogOnMount from 'components/_hoc/withLogOnMount';
+import withLogOnMount from "components/_hoc/withLogOnMount";
 
 // importing assets
-import settings from 'assets/locationInstructions/settings.svg';
-import privacy from 'assets/locationInstructions/privacy.svg';
-import locationServices from 'assets/locationInstructions/locationServices.svg';
-import chrome1 from 'assets/locationInstructions/chrome1.svg';
-import chrome2 from 'assets/locationInstructions/chrome2.svg';
-import safari1 from 'assets/locationInstructions/safari1.svg';
-import safari2 from 'assets/locationInstructions/safari2.svg';
-import opera1 from 'assets/locationInstructions/opera1.svg';
-import opera2 from 'assets/locationInstructions/opera2.svg';
-import firefox1 from 'assets/locationInstructions/firefox1.svg';
-import firefox2 from 'assets/locationInstructions/firefox2.svg';
+import settings from "assets/locationInstructions/settings.svg";
+import privacy from "assets/locationInstructions/privacy.svg";
+import locationServices from "assets/locationInstructions/locationServices.svg";
+import chrome1 from "assets/locationInstructions/chrome1.svg";
+import chrome2 from "assets/locationInstructions/chrome2.svg";
+import safari1 from "assets/locationInstructions/safari1.svg";
+import safari2 from "assets/locationInstructions/safari2.svg";
+import opera1 from "assets/locationInstructions/opera1.svg";
+import opera2 from "assets/locationInstructions/opera2.svg";
+import firefox1 from "assets/locationInstructions/firefox1.svg";
+import firefox2 from "assets/locationInstructions/firefox2.svg";
+import a_apps1 from "assets/locationInstructions/a_apps1.svg";
+import a_apps2 from "assets/locationInstructions/a_apps2.svg";
+import a_chrome1 from "assets/locationInstructions/a_chrome1.svg";
+import a_chrome2 from "assets/locationInstructions/a_chrome2.svg";
+import a_firefox1 from "assets/locationInstructions/a_firefox1.svg";
+import a_firefox2 from "assets/locationInstructions/a_firefox2.svg";
+import a_opera1 from "assets/locationInstructions/a_opera1.svg";
+import a_opera2 from "assets/locationInstructions/a_opera2.svg";
 
 class Instructions extends Component {
   renderProblem() {
     return (
       <div className="row">
         <div className="col s10 offset-s1 grey-text location-problem">
-          <b>This browser doesn't have access to your location :(</b>
+          <b>This browser doesn't have access to your location</b>
           <br />
           <div className="grey lighten-4 grey-text location-note">
-            Circlemesh uses location services to show you active events nearby.{' '}
-            <b>
-              It does not save or track your location after the event is over.
-            </b>
+            Circlemesh uses location services to show you active events nearby.{" "}
+            <br />
+            <span className="blue-text text-lighten-1">
+              Circlemesh does not save or track your location after the event is
+              over!
+            </span>
           </div>
         </div>
       </div>
@@ -46,30 +56,44 @@ class Instructions extends Component {
   }
 
   renderSolutionSteps() {
-    let browser, browser1, browser2;
+    let STEPS, browser, browser1, browser2;
     if (isAndroid) {
-      browser = 'Chrome';
-      browser1 = chrome1;
-      browser2 = chrome2;
+      if (isChrome) {
+        browser = "Chrome";
+        browser1 = a_chrome1;
+        browser2 = a_chrome2;
+      } else if (isFirefox) {
+        browser = "Firefox";
+        browser1 = a_firefox1;
+        browser2 = a_firefox2;
+      } else if (isOpera) {
+        browser = "Opera";
+        browser1 = a_opera1;
+        browser2 = a_opera2;
+      } else {
+        browser = "this browser";
+        browser1 = a_chrome1;
+        browser2 = a_chrome2;
+      }
     } else {
       if (isChrome) {
-        browser = 'Chrome';
+        browser = "Chrome";
         browser1 = chrome1;
         browser2 = chrome2;
       } else if (isFirefox) {
-        browser = 'Firefox';
+        browser = "Firefox";
         browser1 = firefox1;
         browser2 = firefox2;
       } else if (isOpera) {
-        browser = 'Opera';
+        browser = "Opera";
         browser1 = opera1;
         browser2 = opera2;
       } else if (isSafari) {
-        browser = 'Safari';
+        browser = "Safari";
         browser1 = safari1;
         browser2 = safari2;
       } else {
-        browser = 'this browser';
+        browser = "this browser";
         browser1 = safari1;
         browser2 = safari2;
       }
@@ -78,8 +102,8 @@ class Instructions extends Component {
     const iOS_steps = [
       {
         id: 1,
-        text: 'Make sure location services are turned on',
-        path: 'Settings > Privacy > Location Services',
+        text: "Make sure location services are turned on",
+        path: "Settings > Privacy > Location Services",
         img: [settings, privacy, locationServices]
       },
       {
@@ -90,7 +114,28 @@ class Instructions extends Component {
       }
     ];
 
-    return iOS_steps.map(step => {
+    const android_steps = [
+      {
+        id: 1,
+        text: `Go to Settings and then select ${browser}`,
+        path: `Settings > Apps > ${browser}`,
+        img: [a_apps1, browser1]
+      },
+      {
+        id: 2,
+        text: `Select Permissions and turn Location on`,
+        path: `${browser} > Permissions > Location`,
+        img: [a_apps2, browser2]
+      }
+    ];
+
+    if (isAndroid) {
+      STEPS = android_steps;
+    } else {
+      STEPS = iOS_steps;
+    }
+
+    return STEPS.map(step => {
       return (
         <div
           key={step.id}
@@ -104,7 +149,12 @@ class Instructions extends Component {
           <p>{step.path}</p>
           <div className="center">
             {step.img.map(img => {
-              return <img key={img} src={img} alt="" />;
+              return (
+                <div key={img}>
+                  <img src={img} alt="" />
+                  <div className="divider" />
+                </div>
+              );
             })}
           </div>
         </div>
@@ -142,7 +192,7 @@ class Instructions extends Component {
 const logProps = ownProps => {
   return {
     logType: L.MOUNT,
-    componentServed: 'locationError-instructions'
+    componentServed: "locationError-instructions"
   };
 };
 
