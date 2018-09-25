4f83781b05b62121be7993444f49c6a3f68c02e7
Chaitanya <circlemesh@gmail.com>
2018-09-24 20:05:31 -0700

    mobile app redesign + google analytics

--- client/src/components/Wrapper.js
+++ client/src/components/Wrapper.js
@@ -36,6 +36,11 @@ ReactGA.pageview("/signinWithLinkedin");
 ReactGA.pageview("/mesh");
 ReactGA.pageview("/mesh/:meshId");
 ReactGA.pageview("/mesh/:meshId/list");
+ReactGA.pageview("/web/about");
+ReactGA.pageview("/web/form/signin");
+ReactGA.pageview("/web/form/create");
+ReactGA.pageview("/manage/host");
+ReactGA.pageview("/manage/app");
 
 class ComponentsWrapper extends Component {
   render() {
