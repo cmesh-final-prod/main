8278fdaa5f943918a84274820f44795d63dc4c22
Chaitanya <circlemesh@gmail.com>
2018-09-25 03:36:43 -0700

    PUBNUB back to staging

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   // test-prod
-  // pubnubSubscribeKey: "sub-c-c280e022-8b3f-11e8-bb1a-4ed21a5fe7f3"
+  pubnubSubscribeKey: "sub-c-c280e022-8b3f-11e8-bb1a-4ed21a5fe7f3"
   // final-prod
-  pubnubSubscribeKey: "sub-c-6e4a7f0e-be9d-11e8-a0ae-c2e67adadb66"
+  // pubnubSubscribeKey: "sub-c-6e4a7f0e-be9d-11e8-a0ae-c2e67adadb66"
 };
